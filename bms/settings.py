e26806d6dd5e945c5d1c2b1be8941a0de63e1803
codegeek <usmanxp12@gmail.com>
2022-11-30 10:10:40 +0100

    fixed static

--- bms/settings.py
+++ bms/settings.py
@@ -129,7 +129,10 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
